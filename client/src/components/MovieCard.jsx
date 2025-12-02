ef9af4f21f337225ed26640154bebe0034758eaf
imsathishjk <sathishjk01@gmail.com>
2025-12-03 00:05:04 +0530

    Style updated

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ const MovieCard = ({ category, data }) => {
                 {data ?
                     data.map((d, i) => {
                         return <img onClick={() => navigate(`/netflix/${d.id}`, scrollTo(0, 0))} key={i} src={d.poster_path ? `https://image.tmdb.org/t/p/w500${d.poster_path}` : noimage} alt=""
-                            className='hover:scale-105 cursor-pointer transition-all duration-300 ease-in-out w-56 rounded-md border-2 border-gray-600' />
+                            className='hover:scale-105 cursor-pointer transition-all duration-300 ease-in-out w-36 md:w-40 lg:w-48 xl:w-56 rounded-md border-2 border-gray-600' />
                     })
                     : <Loading />}
             </div>
